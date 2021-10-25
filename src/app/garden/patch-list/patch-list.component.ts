0d7881b7851e2acf226e22d44310eb6f8923041a
ukales <audrey_lescot@waters.com>
2021-10-25 09:51:58 +0100

    Add plant list in more info button in patch

--- src/app/garden/patch-list/patch-list.component.ts
+++ src/app/garden/patch-list/patch-list.component.ts
@@ -1,16 +1,33 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input } from '@angular/core';
 import { IPatch } from '../garden-footprint/models/ipatch-model';
+
+
 
 @Component({
   selector: 'app-patch-list',
   templateUrl: './patch-list.component.html',
-  styleUrls: ['./patch-list.component.css']
+  styleUrls: ['./patch-list.component.css'],
+
+  animations: [
+    trigger('bodyExpansion', [
+      state('collapsed, void', style({ height: '0px', visibility: 'hidden' })),
+      state('expanded', style({ height: '*', visibility: 'visible' })),
+      transition('expanded <=> collapsed, void => collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ])
+  ]
 })
 export class PatchListComponent {
   @Input() patch!: IPatch;
 
+  state = 'collapsed';
+
   constructor() { }
 
+  toggle(): void {
+    this.state = this.state === 'collapsed' ? 'expanded' : 'collapsed';
 
+  }
 
 }
