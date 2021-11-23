b9705b6edbe5738cc3e903c5f2d9b4284b4029ee
ukales <audrey_lescot@waters.com>
2021-11-23 16:10:14 +0000

    added css for edit patch

--- src/app/garden/models/ipatch-model.ts
+++ src/app/garden/models/ipatch-model.ts
@@ -1,7 +1,9 @@
 import { IPlantsList } from "src/app/garden-list/models/iplants-model";
+import { ITask } from "src/app/task/models/itask-model";
 
 export interface IPatch {
   name: string,
   icon: string
   plantlist?: IPlantsList[];
+  tasklist?: ITask[];
  }
