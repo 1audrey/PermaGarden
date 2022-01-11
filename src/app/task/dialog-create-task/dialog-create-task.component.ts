c6bd69481be1247b3277551a718c19a0347d63ec
ukales <audrey_lescot@waters.com>
2022-01-11 12:58:26 +0000

    Added logic goToPlantsPage() if patch selected has an empty plant list.

--- src/app/task/dialog-create-task/dialog-create-task.component.ts
+++ src/app/task/dialog-create-task/dialog-create-task.component.ts
@@ -3,6 +3,8 @@ import { FormControl, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { IPatch } from 'src/app/garden/models/ipatch-model';
+import { PatchListComponent } from 'src/app/garden/patch-list/patch-list.component';
+import { NotificationsService } from 'src/app/shared/notifications.service';
 import { PatchesService } from 'src/app/shared/patches.service';
 
 @Component({
@@ -13,16 +15,18 @@ import { PatchesService } from 'src/app/shared/patches.service';
 export class DialogCreateTaskComponent implements OnInit {
 
   public static readonly CREATEPATCH_WEBSITE_URL: string = '/create-patch';
-  // public static readonly CREATETASK_WEBSITE_URL: string = `/create-task', ${this.patchName}`;
+  public static readonly PLANTS_LIST_WEBSITE_URL: string = '/plants-list';
 
   patchControl = new FormControl('', Validators.required);
   patchName!: string;
   public isLoading: boolean = false;
   patches!: IPatch[];
+  plantListEmpty: boolean = false;
 
   constructor(public dialogRef: MatDialogRef<DialogCreateTaskComponent>,
     private patchService: PatchesService,
-    private router: Router) { }
+    private router: Router,
+    private notifications: NotificationsService) { }
 
   ngOnInit(): void {
     this.patchService.getPatch().subscribe(allPatches => {
@@ -35,14 +39,28 @@ export class DialogCreateTaskComponent implements OnInit {
     this.dialogRef.close();
   }
 
-  createPatch(){
-  const link = document.createElement('a');
-  link.href = DialogCreateTaskComponent.CREATEPATCH_WEBSITE_URL;
-  link.setAttribute('visibility', 'hidden');
-  link.click();
+  createPatch() {
+    const link = document.createElement('a');
+    link.href = DialogCreateTaskComponent.CREATEPATCH_WEBSITE_URL;
+    link.setAttribute('visibility', 'hidden');
+    link.click();
   }
 
-  createTaskPage(patchName: string){
+  goToPlantsPage() {
+    this.dialogRef.close();
+    const link = document.createElement('a');
+    link.href = DialogCreateTaskComponent.PLANTS_LIST_WEBSITE_URL;
+    link.setAttribute('visibility', 'hidden');
+    link.click();
+  }
+
+  createTaskPage(patchName: string) {
+    for (let patch of this.patches) {
+      if (patch.name === patchName && !patch.plantlist?.length) {
+        this.notifications.showWarning(`You need to add plants to ${patchName} before creating tasks`);
+        return;
+      }
+    }
     this.router.navigate(['/create-task', patchName])
     this.dialogRef.close();
   }
