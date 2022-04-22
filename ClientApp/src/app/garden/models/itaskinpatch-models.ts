b50677507862a5eaf0763707436dd90ebbf81502
ukales <audrey_lescot@waters.com>
2022-04-22 08:42:35 +0100

    Update summary and fix css on manage tasks component

--- ClientApp/src/app/garden/models/itaskinpatch-models.ts
+++ ClientApp/src/app/garden/models/itaskinpatch-models.ts
@@ -1,6 +1,3 @@
-import { IPlantsList } from "src/app/garden-list/models/iplants-model";
-import { IPlantInPatch } from "./iplantinpatch-model";
-
 export interface ITaskInPatch {
   patchId: number;
   patchName: string;
--- ClientApp/src/app/homepage/summary/summary.component.ts
+++ ClientApp/src/app/homepage/summary/summary.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IPlantsList } from 'src/app/garden-list/models/iplants-model';
 import { IPatch } from 'src/app/garden/models/ipatch-model';
-
 import { ITask } from 'src/app/task/models/itask-model';
-import { PatchesService } from '../../services/patches/patches.service';
-import { PlantsService } from '../../services/plants/plants.service';
 
 @Component({
   selector: 'app-summary',
